601dd647429d7afa31dec4d50a9c7b0f54f4d49a
terrnit <terrnit.megaman@gmail.com>
2022-06-05 10:28:30 +0500

    Style input and add-button

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -6,8 +6,17 @@ function AddTodo() {
     return (
         <form onSubmit={handleSubmit}>
             <HStack>
-                <Input />
-                <Button> AddTodo </Button>
+                <Input 
+                    variant= 'filled'
+                    placeholder='what today ill going to-do ..'
+                    p="5"
+                    
+                />
+                <Button
+                    bg= 'green.400'
+                    p='5'
+                    borderRadius= 'xl'
+                > AddTodo </Button>
             </HStack>
         </form>
     )
