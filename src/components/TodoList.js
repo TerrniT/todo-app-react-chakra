3e4630b0137cd30e3fcda8292397f22ea19bc9eb
terrnit <terrnit.megaman@gmail.com>
2022-06-05 09:58:56 +0500

    add todo [] / create vertical and horisontal stack for ToDoList body

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import { HStack, VStack } from '@chakra-ui/react'
 
 function TodoList() {
-  return (
-    <div>
-        
-    </div>
-  )
+    const todos = [
+        {
+            id: 1,
+            body: 'get bread',
+        },
+        {
+            id: 2,
+            body: 'get pussy',
+        },
+        {
+            id: 3,
+            body: 'start to code',
+        },
+    ]
+    return (
+        <VStack>
+            {todos.map(todo=>(
+                <HStack>
+
+                </HStack>
+            ))}
+        </VStack>
+    )
 }
 
 export default TodoList
