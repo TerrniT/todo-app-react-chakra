e989a9fcd44c99148186cf7b3a9e1d9ac2c8cb7f
terrnit <terrnit.megaman@gmail.com>
2022-06-05 09:53:09 +0500

    Style heading / add react-icon library and import light and dark mode icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import {Heading} from '@chakra-ui/react'
+import { Heading, VStack, IconButton } from '@chakra-ui/react'
 import './App.css';
-import TodoList from './components/TodoList'
-import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+import { FaSun, FaMoon } from 'react-icons/fa'
 
 
 function App() {
   return (
-    <Heading>Todo Application</Heading>
+    <VStack p={10}>
+      <IconButton icon={<FaSun />}
+        isRound='true'
+        size='lg'
+        alignSelf='flex-end'
+      />
+      <Heading
+        mb='8'
+        fontWeight='extrabold'
+        size='2xl'
+        alignSelf='flex-start'
+        bgGradient='linear(to-r, green.500, teal.400)'
+        bgClip='text'
+      >
+        Todo Application
+      </Heading>
+    </VStack>
   );
 }
 
