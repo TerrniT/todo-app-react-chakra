4ec8dca0e501ebb8e70eb527a411399e13b14083
terrnit <terrnit.megaman@gmail.com>
2022-06-05 10:59:39 +0500

    update handleSubmit function - create dummy content handler with todo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,17 +1,27 @@
 import { Button, HStack, Input } from '@chakra-ui/react'
 import React from 'react'
+import {useState} from 'react'
 
 function AddTodo({AddTodo}) {
     function handleSubmit(e) { 
         e.preventDefault();
-        console.log('123');
+        console.log(content);
+        const todo = {
+            id: '',
+            body: content,
+        };
     }
+
+    const [content, setContent] = useState('');
+
     return (
         <form onSubmit={handleSubmit}>
             <HStack>
                 <Input 
                     variant= 'filled'
                     placeholder='what today ill going to-do ..'
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
                     p="5"
                 />
                 <Button
